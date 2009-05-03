2028aedae48e5158c36926b447c3fc627ddf8f6e
SamuraiJack <nickolay8@gmail.com>
2009-05-03 22:17:39 +0400

    - implemented sibling browsers killing

--- lib/JSAN/Prove/App/Process.pm
+++ lib/JSAN/Prove/App/Process.pm
@@ -1,52 +1,69 @@
-package JSAN::Prove::App;
+package JSAN::Prove::App::Process;
 
-use Module::Pluggable sub_name => 'available_browsers', search_path => [ 'JSAN::Prove::App::Browser' ], require => 1;
+if($^O eq 'MSWin32') {
+    require Win32::Process::List;
+    require Win32::Process;
+    
+} else {
+    require Proc::ProcessTable;
+}
 
-use Moose;
+use Proc::Exists qw(pexists);
 
-has 'browsers' => ( is => 'rw' );
 
- 
 #================================================================================================================================================================================================================================================
 #================================================================================================================================================================================================================================================
-sub start_browsers {
-	my ($self, $url) = @_;
-	
-	my @startable_browsers = grep { $_->is_startable } $self->available_browsers();
-	
-	my $browsers = {};
-	foreach my $class ( @startable_browsers ) {
-		$class =~ m/JSAN::Prove::App::Browser::(.+)/; 
-		$browsers->{$1} = $class->new;
-		die "Startable browser [$class] failed to start" unless $browsers->{$1}->start($url . "/prove/$1");
-	};
-	
-	$self->browsers($browsers);
-	
-	die "No browsers were able to start on this platform" unless keys(%$browsers); 
+sub kill_by_name {
+    my ($self, $name_regex) = @_;
+    
+    my %name_pid_hash = $self->get_name_pid_hash;
+    
+    foreach my $pid (keys(%name_pid_hash)) {
+    	$self->kill_by_pid($pid) if $name_pid_hash{$pid} =~ /$name_regex/; 
+    }
 }
 
 
 #================================================================================================================================================================================================================================================
 #================================================================================================================================================================================================================================================
-sub stop_browser {
-	my ($self, $browser) = @_;
-	
-	$self->browsers->{$browser}->stop();
-}
-
-
-#================================================================================================================================================================================================================================================
-#================================================================================================================================================================================================================================================
-sub is_running {
-	my ($self) = @_;
-	
-	my $running = 0;
-	foreach (values(%{$self->browsers})) {
-		$running = 1 if $_->is_alive;
+sub get_name_pid_hash {
+	if($^O eq 'MSWin32') {
+		return Win32::Process::List->new->GetProcesses;
+	} else {
+	    my $process_list = Proc::ProcessTable->new->table;
+	    
+	    my %name_pid_hash = ();
+	    foreach $process (@$process_list) {
+	    	$name_pid_hash{$process->pid} = $process->cmndline;
+	    }
+	    
+	    return %name_pid_hash;
 	}
+}
+
+
+#================================================================================================================================================================================================================================================
+#================================================================================================================================================================================================================================================
+sub kill_by_pid {
+	my ($self, $pid) = @_;
 	
-	return $running;
+	if($^O eq 'MSWin32') {
+		my $exit_code;
+		
+		return Win32::Process::KillProcess($pid, $exit_code)
+	} else {
+		
+		foreach my $signal (qw(HUP INT QUIT KILL)) {
+			my $count = 5;
+			
+			while ($count and pexists($pid)) {
+				--$count;
+				kill($signal, $pid);
+				last SIGNAL unless pexists($pid);
+				sleep 1;
+			}
+		}
+	}
 }
 
 
--- t/01app.t
+++ t/01app.t
@@ -3,14 +3,40 @@ use warnings;
 use FindBin;
 use lib "$FindBin::Bin/../lib";
 
-use Test::More tests => 3;
+use Test::More tests => 5;
 
 BEGIN { use_ok 'Catalyst::Test', 'JSAN::Prove', 'JSAN::Prove::App' }
+
+#================================================================================================================================================================================================================================================
+diag('Web-server');
 
 ok( request('/')->is_success, 'Request should succeed' );
 
 
+
 #================================================================================================================================================================================================================================================
+diag('Application creation & browsers starting');
+
 my $app = JSAN::Prove::App->new;
 
 ok($app, 'App instance was created');
+
+$app->start_browsers('http://yandex.ru');
+
+sleep(2);
+
+ok($app->is_running, 'At least one browser started');
+
+sleep(2);
+
+
+#================================================================================================================================================================================================================================================
+diag('Browsers stopping');
+
+my @browsers = keys(%{$app->browsers});
+
+foreach my $browser (@browsers) {
+	$app->stop_browser($browser);
+}
+
+ok(!$app->is_running, 'All browsers closed');
